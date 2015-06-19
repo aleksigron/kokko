e4888ac834306083fac5e72ef2bd670e7df485e6
Aleksi Grön <aleksi@Aleksis-MacBook-Pro.local>
2015-06-20 01:25:58 +0300

    Initial Commit

---
+++ GraphicsToolkit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GraphicsToolkit
+//
+//  Created by Aleksi Grön on 20/06/15.
+//  Copyright (c) 2015 Aleksi Grön. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
