ca7167d46f0c30d767892ba5c6baef551b80b72b
Aleksi Grön <aleksi.gron@gmail.com>
2015-10-10 23:32:55 +0300

    Try out a different lighting model

--- GraphicsToolkit/res/shaders/lighting.fragment.glsl
+++ GraphicsToolkit/res/shaders/lighting.fragment.glsl
@@ -1,10 +1,15 @@
 #version 330 core
 
-in vec3 vert_color;
+in vec3 fs_w_norm;
+in vec3 fs_vert_color;
 
 out vec3 color;
 
 void main()
 {
-	color = vert_color;
+	vec3 light_dir = vec3(0.0, 1.0, 0.0);
+	float f = dot(fs_w_norm, light_dir);
+	f = (f + 1.0) * 0.5;
+
+	color = fs_vert_color * f;
 }
