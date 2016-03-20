73f8e306d08136c4cc031df8609577778e4fd26e
Aleksi Grön <aleksi.gron@gmail.com>
2016-03-21 00:01:36 +0200

    Try a shader with two fixed lights

--- res/shaders/lighting.fragment.glsl
+++ res/shaders/lighting.fragment.glsl
@@ -6,8 +6,13 @@ out vec3 color;
 
 void main()
 {
-	vec3 light_dir = vec3(0.577, 0.577, 0.577);
-	float f = dot(fs_w_norm, light_dir);
+	vec3 light_0_dir = vec3(0.577, 0.577, 0.577);
+	vec3 light_0_col = vec3(1.0, 1.0, 1.0);
+	float intensity_0 = clamp(dot(fs_w_norm, light_0_dir), 0.0, 1.0);
 
-	color = vec3(1.0, 1.0, 1.0) * f;
+	vec3 light_1_dir = vec3(-0.577, -0.577, -0.577);
+	vec3 light_1_col = vec3(0.45, 0.45, 0.5);
+	float intensity_1 = clamp(dot(fs_w_norm, light_1_dir), 0.0, 1.0);
+
+	color = light_0_col * intensity_0 + light_1_col * intensity_1;
 }
