962590a61f0e980f992444068aed4f3b0ebfa437
Aleksi Grön <aleksi.gron@gmail.com>
2016-03-30 21:48:16 +0300

    Correctly normalize input normal in fragment shader

--- res/shaders/lighting.vertex.glsl
+++ res/shaders/lighting.vertex.glsl
@@ -3,7 +3,7 @@
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 normal;
 
-out vec3 fs_w_norm;
+out vec3 fs_v_norm;
 
 uniform mat4 _MVP;
 uniform mat4 _MV;
@@ -11,5 +11,5 @@ uniform mat4 _MV;
 void main()
 {
 	gl_Position = _MVP * vec4(position, 1.0);
-	fs_w_norm = (_MV * vec4(normal, 0.0)).xyz;
+	fs_v_norm = (_MV * vec4(normal, 0.0)).xyz;
 }
