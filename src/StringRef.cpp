cbafea4d718b42c9ff85efdf18590ad8dd5ffef5
Aleksi Grön <aleksi.gron@gmail.com>
2016-06-07 00:28:00 +0300

    Move some stuff to StringRef.cpp

---
+++ src/StringRef.cpp
@@ -0,0 +1,51 @@
+#include "StringRef.hpp"
+
+bool StringRef::ValueEquals(const StringRef& other) const
+{
+	if (len != other.len || str == nullptr || other.str == nullptr)
+		return false;
+
+	for (unsigned i = 0; i < len; ++i)
+		if (str[i] != other.str[i])
+			return false;
+
+	return true;
+}
+
+bool StringRef::ValueEquals(const char* cstring) const
+{
+	if (this->str == nullptr || cstring == nullptr)
+		return false;
+
+	unsigned int i = 0;
+	while (i < this->len)
+	{
+		if (cstring[i] == '\0' || this->str[i] != cstring[i])
+			return false;
+
+		++i;
+	}
+
+	// The strings match for the length of this StringRef
+	// If cstring too ends here, the strings are equal
+	return cstring[i] == '\0';
+}
+
+void StringRef::TrimBeginning(unsigned int amount)
+{
+	if (amount < len)
+	{
+		str = str + amount;
+		len = len - amount;
+	}
+	else
+		this->Invalidate();
+}
+
+void StringRef::TrimEnd(unsigned int amount)
+{
+	if (amount < len)
+		len = len - amount;
+	else
+		this->Invalidate();
+}
--- src/StringRef.hpp
+++ src/StringRef.hpp
@@ -6,88 +6,45 @@ struct StringRef
 	unsigned int len;
 
 	StringRef() : str(nullptr), len(0) {}
+
 	StringRef(const char* string, unsigned int length) : str(string), len(length) {}
 
 	explicit StringRef(const char* string) : str(string)
 	{
-		const char* itr = str;
-
-		while (*itr != '\0')
+		while (*string != '\0')
 		{
-			++itr;
+			++string;
 		}
 
-		len = static_cast<unsigned int>(itr - str);
+		len = static_cast<unsigned int>(string - str);
 	}
 
-	inline bool ReferenceEquals(const StringRef& other) const
-	{ return this->str == other.str && this->len == other.len; }
-
-	bool ValueEquals(const StringRef& other) const
+	bool ReferenceEquals(const StringRef& other) const
 	{
-		if (len != other.len || str == nullptr || other.str == nullptr)
-			return false;
-
-		for (unsigned i = 0; i < len; ++i)
-			if (str[i] != other.str[i])
-				return false;
-
-		return true;
+		return this->str == other.str && this->len == other.len;
 	}
 
-	bool ValueEquals(const char* cstring) const
+	bool ValueEquals(const StringRef& other) const;
+	bool ValueEquals(const char* cstring) const;
+
+	bool operator == (const StringRef& other) const
 	{
-		if (this->str == nullptr || cstring == nullptr)
-			return false;
-
-		unsigned int i = 0;
-		while (i < this->len)
-		{
-			if (cstring[i] == '\0' || this->str[i] != cstring[i])
-				return false;
-
-			++i;
-		}
-
-		// The strings match for the length of this StringRef
-		// If cstring too ends here, the strings are equal
-		return cstring[i] == '\0';
+		return this->ValueEquals(other);
 	}
 
-	inline bool operator == (const StringRef& other) const
-	{ return this->ValueEquals(other); }
+	bool operator != (const StringRef& other) const
+	{
+		return this->ValueEquals(other) == false;
+	}
 
-	inline bool operator != (const StringRef& other) const
-	{ return this->ValueEquals(other) == false; }
+	bool IsValid() const { return str != nullptr; }
 
-	inline bool IsValid() const
-	{ return str != nullptr; }
-
-	inline operator bool() const
-	{ return this->IsValid(); }
-
-	inline void Invalidate()
+	void Invalidate()
 	{
 		str = nullptr;
 		len = 0;
 	}
 
-	void TrimBeginning(unsigned int amount)
-	{
-		if (amount < len)
-		{
-			str = str + amount;
-			len = len - amount;
-		}
-		else
-			this->Invalidate();
-	}
-
-	void TrimEnd(unsigned int amount)
-	{
-		if (amount < len)
-			len = len - amount;
-		else
-			this->Invalidate();
-	}
+	void TrimBeginning(unsigned int amount);
+	void TrimEnd(unsigned int amount);
 };
