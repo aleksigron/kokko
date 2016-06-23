23acbd80f3cdd8f4cae144c58ff2d87cbb2fd8b0
Aleksi Grön <aleksi.gron@gmail.com>
2016-06-24 00:40:46 +0300

    Add Color and ColorSrgb classes

---
+++ src/Color.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+struct Color
+{
+	float r, g, b, a;
+
+	Color()
+	{
+	}
+
+	Color(float r, float g, float b) : r(r), g(g), b(b), a(1.0f)
+	{
+	}
+
+	Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a)
+	{
+	}
+
+	Color& operator*=(const Color& value)
+	{
+		this->r *= value.r;
+		this->g *= value.g;
+		this->b *= value.b;
+		this->a *= value.a;
+
+		return *this;
+	}
+
+	Color& operator*=(float value)
+	{
+		this->r *= value;
+		this->g *= value;
+		this->b *= value;
+		this->a *= value;
+
+		return *this;
+	}
+
+	Color& operator+=(const Color& value)
+	{
+		this->r += value.r;
+		this->g += value.g;
+		this->b += value.b;
+		this->a += value.a;
+
+		return *this;
+	}
+
+	Color& operator-=(const Color& value)
+	{
+		this->r -= value.r;
+		this->g -= value.g;
+		this->b -= value.b;
+		this->a -= value.a;
+
+		return *this;
+	}
+};
+
+inline Color operator*(const Color& lhs, float rhs)
+{
+	return Color(lhs.r * rhs, lhs.g * rhs, lhs.b * rhs, lhs.a * rhs);
+}
+
+inline Color operator*(float lhs, const Color& rhs)
+{
+	return operator*(rhs, lhs);
+}
+
+inline Color operator*(const Color& lhs, const Color& rhs)
+{
+	return Color(lhs.r * rhs.r, lhs.g * rhs.g, lhs.b * rhs.b, lhs.a * rhs.a);
+}
+
+inline Color operator+(const Color& lhs, const Color& rhs)
+{
+	return Color(lhs.r + rhs.r, lhs.g + rhs.g, lhs.b + rhs.b, lhs.a + rhs.a);
+}
+
+inline Color operator-(const Color& lhs, const Color& rhs)
+{
+	return Color(lhs.r - rhs.r, lhs.g - rhs.g, lhs.b - rhs.b, lhs.a - rhs.a);
+}
---
+++ src/ColorSrgb.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <cmath>
+
+#include "Color.hpp"
+
+struct ColorSrgb
+{
+	using Component = unsigned char;
+
+	unsigned char r, g, b, a;
+
+	ColorSrgb() : r(0), g(0), b(0), a(0)
+	{
+	}
+
+	ColorSrgb(const ColorSrgb& other) : r(other.r), g(other.g), b(other.b), a(other.a)
+	{
+	}
+
+	ColorSrgb(const Color& linear)
+	{
+		this->FromLinear(linear);
+	}
+
+	ColorSrgb(unsigned char r, unsigned char g, unsigned char b) :
+		r(r), g(g), b(b), a(255)
+	{
+	}
+
+	ColorSrgb(unsigned char r, unsigned char g, unsigned char b, unsigned char a) :
+		r(r), g(g), b(b), a(a)
+	{
+	}
+
+	Color ToLinear()
+	{
+		Color linear;
+		linear.r = std::pow(r / 255.0f, 1.0f / 2.2f);
+		linear.g = std::pow(g / 255.0f, 1.0f / 2.2f);
+		linear.b = std::pow(b / 255.0f, 1.0f / 2.2f);
+		linear.a = a / 255.0f;
+
+		return linear;
+	}
+
+	void FromLinear(const Color& linear)
+	{
+		this->r = static_cast<Component>(std::pow(linear.r, 2.2f) * 255.99998474f);
+		this->g = static_cast<Component>(std::pow(linear.g, 2.2f) * 255.99998474f);
+		this->b = static_cast<Component>(std::pow(linear.b, 2.2f) * 255.99998474f);
+		this->a = static_cast<Component>(linear.a * 255.0f);
+	}
+};
