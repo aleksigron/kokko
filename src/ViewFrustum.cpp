d2b02bb73354806a30112d02f6c471f5dcf1ce32
Aleksi Grön <aleksi.gron@gmail.com>
2019-03-01 20:29:37 +0200

    ViewFrustum calculates corner points

--- src/ViewFrustum.cpp
+++ src/ViewFrustum.cpp
@@ -19,22 +19,42 @@ void ViewFrustum::UpdateFrustum(const Camera& camera, const Mat4x4f& cameraTrans
 	const float near = camera.nearClipDistance;
 	const float far = camera.farClipDistance;
 
-	const float halfFarHeight = std::tan(camera.perspectiveFieldOfView * 0.5f) * far;
+	const float halfFovTan = std::tan(camera.perspectiveFieldOfView * 0.5f);
+
+	const float halfNearHeight = halfFovTan * near;
+	const float halfNearWidth = halfNearHeight * camera.aspectRatio;
+
+	const float halfFarHeight = halfFovTan * far;
 	const float halfFarWidth = halfFarHeight * camera.aspectRatio;
 
 	const Vec3f farForward = forward * far;
-	const Vec3f farRight = right * halfFarWidth;
-	const Vec3f farUp = up * halfFarHeight;
+	const Vec3f halfFarRight = right * halfFarWidth;
+	const Vec3f halfFarUp = up * halfFarHeight;
 
-	const Vec3f dirTopLeft = farForward - farRight + farUp;
-	const Vec3f dirBottomRight = farForward + farRight - farUp;
+	const Vec3f dirTopLeft = farForward - halfFarRight + halfFarUp;
+	const Vec3f dirBottomRight = farForward + halfFarRight - halfFarUp;
+
+	const Vec3f nearCenter = position + forward * near;
+	const Vec3f farCenter = position + farForward;
 
 	// Order: near, left, top, right, bottom, far
 
-	planes[0].SetPointAndNormal(position + forward * near, forward);
-	planes[1].SetPointAndNormal(position, Vec3f::Cross(dirTopLeft, farUp).GetNormalized());
-	planes[2].SetPointAndNormal(position, Vec3f::Cross(dirTopLeft, farRight).GetNormalized());
-	planes[3].SetPointAndNormal(position, Vec3f::Cross(farUp, dirBottomRight).GetNormalized());
-	planes[4].SetPointAndNormal(position, Vec3f::Cross(farRight, dirBottomRight).GetNormalized());
-	planes[5].SetPointAndNormal(position + farForward, -forward);
+	planes[0].SetPointAndNormal(nearCenter, forward);
+	planes[1].SetPointAndNormal(position, Vec3f::Cross(dirTopLeft, up).GetNormalized());
+	planes[2].SetPointAndNormal(position, Vec3f::Cross(dirTopLeft, right).GetNormalized());
+	planes[3].SetPointAndNormal(position, Vec3f::Cross(up, dirBottomRight).GetNormalized());
+	planes[4].SetPointAndNormal(position, Vec3f::Cross(right, dirBottomRight).GetNormalized());
+	planes[5].SetPointAndNormal(farCenter, -forward);
+
+	const Vec3f halfNearRight = right * halfNearWidth;
+	const Vec3f halfNearUp = up * halfNearHeight;
+
+	points[0] = nearCenter + halfNearUp - halfNearRight; // Near top left
+	points[1] = nearCenter + halfNearUp + halfNearRight; // Near top right
+	points[2] = nearCenter - halfNearUp - halfNearRight; // Near bottom left
+	points[3] = nearCenter - halfNearUp + halfNearRight; // Near bottom right
+	points[4] = farCenter + halfFarUp - halfFarRight; // Far top left
+	points[5] = farCenter + halfFarUp + halfFarRight; // Far top right
+	points[6] = farCenter - halfFarUp - halfFarRight; // Far bottom left
+	points[7] = farCenter - halfFarUp + halfFarRight; // Far bottom right
 }
--- src/ViewFrustum.hpp
+++ src/ViewFrustum.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Plane.hpp"
+#include "Vec3.hpp"
 
 class Camera;
 struct Mat4x4f;
@@ -9,6 +10,7 @@ struct BoundingBox;
 struct ViewFrustum
 {
 	Plane planes[6];
+	Vec3f points[8];
 
 	void UpdateFrustum(const Camera& camera, const Mat4x4f& cameraTransform);
 };
