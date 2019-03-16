fb36ee5b0b1bd9386b7750d3850f88ad578f8ac9
Aleksi Grön <aleksi.gron@gmail.com>
2019-03-16 17:07:11 +0200

    Reorganize blender_custom_format
    - Move current version of script to folder 2.63
    - Add almost compatible version of script to folder 2.80

---
+++ scripts/blender_custom_format/2.80/__init__.py
@@ -0,0 +1,67 @@
+bl_info = {
+    "name": "Custom mesh export",
+    "description": "Export mesh to custom file format",
+    "author": "Aleksi Grön",
+    "version": (0, 2),
+    "blender": (2, 80, 0),
+    "location": "File > Export",
+    "category": "Import-Export"
+}
+
+import bpy
+from bpy.props import StringProperty, BoolProperty
+from bpy_extras.io_utils import ExportHelper
+
+class CUSTOM_OT_export_format(bpy.types.Operator, ExportHelper):
+    """Custom format mesh export script"""
+    bl_idname = "export_mesh.gtk_mesh"
+    bl_label = "Export custom mesh"
+    
+    filename_ext = ".mesh"
+    filter_glob = StringProperty(default="*.mesh", options={'HIDDEN'})
+    
+    apply_modifiers: BoolProperty(
+        name = "Apply modifiers",
+        default = True)
+    
+    save_normal: BoolProperty(
+        name = "Save vertex normals",
+        default = True)
+        
+    save_tex_coord: BoolProperty(
+        name = "Save texture coords (if available)",
+        default = True)
+        
+    save_vert_color: BoolProperty(
+        name = "Save vertex colors (if available)",
+        default = True)
+
+    def execute(self, context):
+        
+        options = {}
+        options["apply_modifiers"] = self.apply_modifiers
+        options["save_normal"] = self.save_normal
+        options["save_tex_coord"] = self.save_tex_coord
+        options["save_vert_color"] = self.save_vert_color
+        
+        from . import export
+        export.write(context, self.filepath, options)
+        
+        return {'FINISHED'}
+
+
+def menuItemFunc_Export(self, context):
+    self.layout.operator(CUSTOM_OT_export_format.bl_idname, text="Custom file (.mesh)")
+
+def register():
+    from bpy.utils import register_class
+    register_class(CUSTOM_OT_export_format)
+    bpy.types.TOPBAR_MT_file_export.append(menuItemFunc_Export)
+
+def unregister():
+    from bpy.utils import unregister_class
+    unregister_class(CUSTOM_OT_export_format)
+    bpy.types.TOPBAR_MT_file_export.remove(menuItemFunc_Export)
+
+if __name__ == "__main__":
+    register()
---
+++ scripts/blender_custom_format/2.80/export.py
@@ -0,0 +1,193 @@
+import bpy
+from array import array
+from struct import pack
+
+def get_bounding_box(verts, bounding_box_data):
+    if len(verts) > 0:
+        min_x = verts[0].co.x
+        max_x = verts[0].co.x
+        min_y = verts[0].co.z
+        max_y = verts[0].co.z
+        min_z = -verts[0].co.y
+        max_z = -verts[0].co.y
+        
+        for v in verts:
+            if v.co.x < min_x: min_x = v.co.x
+            elif v.co.x > max_x: max_x = v.co.x
+            if v.co.z < min_y: min_y = v.co.z
+            elif v.co.z > max_y: max_y = v.co.z
+            if -v.co.y < min_z: min_z = -v.co.y
+            elif -v.co.y > max_z: max_z = -v.co.y
+        
+        # Center X
+        bounding_box_data.append((min_x + max_x) * 0.5)
+        
+        # Center Y
+        bounding_box_data.append((min_y + max_y) * 0.5)
+        
+        # Center Z
+        bounding_box_data.append((min_z + max_z) * 0.5)
+        
+        # Extent X
+        bounding_box_data.append((max_x - min_x) * 0.5)
+        
+        # Extent Y
+        bounding_box_data.append((max_y - min_y) * 0.5)
+        
+        # Extent Z
+        bounding_box_data.append((max_z - min_z) * 0.5)
+        
+    # Empty bounding box
+    else:
+        for i in range(0, 6):
+            bounding_box_data.append(0.0)
+
+def process_mesh(mesh):
+    import bmesh
+    bm = bmesh.new()
+    bm.from_mesh(mesh)
+    bmesh.ops.triangulate(bm, faces = bm.faces)
+    bm.to_mesh(mesh)
+    bm.free()
+
+def write(context, filepath, options):
+    
+    obj = context.active_object
+    
+    if obj.type != 'MESH': return False
+    
+    # Create a new mesh from the active object
+    mesh_data = obj.to_mesh(context.depsgraph, options['apply_modifiers'], 'PREVIEW')
+    
+    # Triangulate mesh copy
+    process_mesh(mesh_data)
+    
+    # Calculate bounding box from processed mesh
+    bounding_box_data = array('f')
+    get_bounding_box(mesh_data.vertices, bounding_box_data)
+    
+    # Get vertex color and texture coordinate layer counts
+    vert_color_count = len(mesh_data.vertex_colors)
+    tex_coord_count = len(mesh_data.uv_layers)
+    
+    # Decide which vertex data components to save
+    save_normal = options['save_normal']
+    save_vert_color = options['save_vert_color'] and vert_color_count > 0
+    save_tex_coord = options['save_tex_coord'] and tex_coord_count > 0
+    
+    # Vertex data components
+    vert_position_comp = 1 << 0 # 1 position
+    vert_normal_comp = 1 << 1 # [0,1] normals
+    vert_color_comp = 1 << 2 # [0,1] colors
+    vert_texcoord_comp = 1 << 3 # [0,1] texture coords
+    
+    # Combine vertex data components to one integer
+    vert_comps = vert_position_comp
+    if save_normal: vert_comps = vert_comps | vert_normal_comp
+    if save_vert_color: vert_comps = vert_comps | vert_color_comp
+    if save_tex_coord: vert_comps = vert_comps | vert_texcoord_comp
+    
+    vert_count = len(mesh_data.vertices)
+    
+    #epsilon = 1 / (2 ** 20)
+    
+    vert_col_data = array('f')
+    
+    if save_vert_color:
+        # Create an empty array of required size
+        for i in range(0, vert_count * 3):
+            vert_col_data.append(0.0)
+        
+        # Shorter name for vertex color data
+        mesh_vert_col = mesh_data.vertex_colors.active
+        
+        # Get the vertex color for each corner of each polygon
+        # Write colors to the corresponding vertex index in the array
+        for i in range(0, len(mesh_data.loops)):
+            v_index = mesh_data.loops[i].vertex_index
+            v_color = mesh_vert_col.data[i].color
+            vert_col_data[v_index * 3 + 0] = v_color[0]
+            vert_col_data[v_index * 3 + 1] = v_color[1]
+            vert_col_data[v_index * 3 + 2] = v_color[2]
+    
+    # Shorter name for mesh vertices
+    verts = mesh_data.vertices
+    
+    # 'f' for 4-byte floating point
+    vertex_data = array('f')
+    
+    # Put vertex data in the array
+    
+    # Just vertex position
+    if vert_comps == vert_position_comp:
+        for v in verts:
+            vertex_data.extend([v.co.x, v.co.z, -v.co.y])
+            
+    # Vertex position and normal
+    elif vert_comps == (vert_position_comp | vert_normal_comp):
+        for v in verts:
+            vertex_data.extend([v.co.x, v.co.z, -v.co.y])
+            vertex_data.extend([v.normal.x, v.normal.z, -v.normal.y])
+    
+    # Vertex position and color
+    elif vert_comps == (vert_position_comp | vert_color_comp):
+        for i in range(0, vert_count):
+            vertex_data.append(verts[i].co.x)
+            vertex_data.append(verts[i].co.z)
+            vertex_data.append(-verts[i].co.y)
+            vertex_data.append(vert_col_data[i * 3 + 0])
+            vertex_data.append(vert_col_data[i * 3 + 1])
+            vertex_data.append(vert_col_data[i * 3 + 2])
+    
+    # Vertex position, normal and color
+    elif vert_comps == (vert_position_comp | vert_normal_comp | vert_color_comp):
+        for i in range(0, vert_count):
+            vertex_data.append(verts[i].co.x)
+            vertex_data.append(verts[i].co.z)
+            vertex_data.append(-verts[i].co.y)
+            vertex_data.append(verts[i].normal.x)
+            vertex_data.append(verts[i].normal.z)
+            vertex_data.append(-verts[i].normal.y)
+            vertex_data.append(vert_col_data[i * 3 + 0])
+            vertex_data.append(vert_col_data[i * 3 + 1])
+            vertex_data.append(vert_col_data[i * 3 + 2])
+    
+    # 'H': unsigned 2-byte int, 'I': unsigned 4-byte int
+    if vert_count <= (1 << 16):
+        index_data = array('H')
+    else:
+        index_data = array('I')
+    
+    # Get index data
+    for p in mesh_data.polygons:
+        index_data.extend([p.vertices[0], p.vertices[1], p.vertices[2]])
+    
+    # Prepare data for the header
+    index_count = len(index_data)
+    
+    # '=': native byte-order, standard packing
+    # 'I': unsigned 4-byte integer
+    header = pack('=III', vert_comps, vert_count, index_count)
+    
+    # Remove the mesh we created earlier
+    bpy.data.meshes.remove(mesh_data)
+
+    # Open output file
+    with open(filepath, 'wb') as outfile:
+
+        # Start with file type magic number
+        outfile.write(b'\x10\x10\x19\x91');
+        
+        # Write header data
+        outfile.write(header)
+        
+        # Write bounding box
+        bounding_box_data.tofile(outfile)
+        
+        # Write vertex data
+        vertex_data.tofile(outfile)
+        
+        # Write index data
+        index_data.tofile(outfile)
+        
+    return True
