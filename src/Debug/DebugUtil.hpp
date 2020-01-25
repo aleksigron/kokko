91617d07147620a652153d401572e565bd9cad6f
Aleksi Grön <aleksi.gron@gmail.com>
2020-01-25 23:34:46 +0200

    Add DebugUtil
    First functionality is to format a uint64_t render command to more understandable string format.

---
+++ src/Debug/DebugUtil.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstdio>
+
+#include "Rendering/RenderOrder.hpp"
+
+namespace DebugUtil
+{
+	void FormatRenderCommand(const RenderOrderConfiguration& orderInfo, uint64_t command, char* outputBuffer)
+	{
+		uint64_t vi = orderInfo.viewportIndex.GetValue(command);
+		uint64_t vp = orderInfo.viewportPass.GetValue(command);
+		uint64_t ic = orderInfo.command.GetValue(command);
+		uint64_t td = orderInfo.transparentDepth.GetValue(command);
+		uint64_t od = orderInfo.opaqueDepth.GetValue(command);
+		uint64_t op = orderInfo.opaquePadding.GetValue(command);
+		uint64_t mi = orderInfo.materialId.GetValue(command);
+		uint64_t ro = orderInfo.renderObject.GetValue(command);
+		uint64_t co = orderInfo.commandOrder.GetValue(command);
+		uint64_t ct = orderInfo.commandType.GetValue(command);
+		uint64_t cd = orderInfo.commandData.GetValue(command);
+
+		if (ic == 1)
+		{
+			if (vp != 0)
+				sprintf(outputBuffer, "%01llx %01llx %01llx %06llx %03llx %05llx", vi, vp, ic, td, mi, ro);
+			else
+				sprintf(outputBuffer, "%01llx %01llx %01llx %06llx %03llx %05llx", vi, vp, ic, od, mi, ro);
+		}
+		else
+			sprintf(outputBuffer, "%01llx %01llx %01llx %01llx %02llx %08llx", vi, vp, ic, co, ct, cd);
+	}
+}
