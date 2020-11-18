b7be1c086241f869c01049562ee6b58def3f6462
Aleksi Grön <aleksi.gron@gmail.com>
2020-11-18 15:42:01 +0200

    Add many more objects to the scene for testing

--- src/Application/App.cpp
+++ src/Application/App.cpp
@@ -1,15 +1,22 @@
 #include "App.hpp"
 
-#include "Math/Math.hpp"
-#include "System/Time.hpp"
-
 #include "Engine/Engine.hpp"
-#include "System/Window.hpp"
-#include "Rendering/Renderer.hpp"
+
 #include "Entity/EntityManager.hpp"
-#include "Scene/SceneManager.hpp"
+
+#include "Math/Math.hpp"
+
+#include "Rendering/Renderer.hpp"
 #include "Rendering/LightManager.hpp"
+
+#include "Resources/MeshManager.hpp"
+#include "Resources/MaterialManager.hpp"
+
 #include "Scene/Scene.hpp"
+#include "Scene/SceneManager.hpp"
+
+#include "System/Time.hpp"
+#include "System/Window.hpp"
 
 App* App::instance = nullptr;
 
@@ -33,6 +40,9 @@ void App::Initialize()
 	Engine* engine = Engine::GetInstance();
 	EntityManager* entityManager = engine->GetEntityManager();
 	LightManager* lightManager = engine->GetLightManager();
+	MaterialManager* materialManager = engine->GetMaterialManager();
+	MeshManager* meshManager = engine->GetMeshManager();
+	Renderer* renderer = engine->GetRenderer();
 
 	this->cameraController.SetControlledCamera(&this->mainCamera);
 
@@ -47,20 +57,59 @@ void App::Initialize()
 	scene->SetActiveCamera(&this->mainCamera);
 	sceneManager->SetPrimarySceneId(sceneId);
 
-	// Create camera entity and components
-	Entity mainCameraEntity = entityManager->Create();
-	mainCamera.SetEntity(mainCameraEntity);
-	SceneObjectId cameraSceneObject = scene->AddSceneObject(mainCameraEntity);
-	Mat4x4f cameraTransform = Mat4x4f::Translate(Vec3f(0.0f, 1.6f, 4.0f));
-	scene->SetLocalTransform(cameraSceneObject, cameraTransform);
-	
-	Window* window = engine->GetMainWindow();
-	Vec2f frameSize = window->GetFrameBufferSize();
-	mainCamera.parameters.projection = ProjectionType::Perspective;
-	mainCamera.parameters.near = 0.1f;
-	mainCamera.parameters.far = 100.0f;
-	mainCamera.parameters.height = Math::DegreesToRadians(45.0f);
-	mainCamera.parameters.SetAspectRatio(frameSize.x, frameSize.y);
+	{
+		// Create camera entity and components
+		Entity mainCameraEntity = entityManager->Create();
+		mainCamera.SetEntity(mainCameraEntity);
+		SceneObjectId cameraSceneObject = scene->AddSceneObject(mainCameraEntity);
+		Mat4x4f cameraTransform = Mat4x4f::Translate(Vec3f(0.0f, 1.6f, 4.0f));
+		scene->SetLocalTransform(cameraSceneObject, cameraTransform);
+	}
+
+	{
+		Window* window = engine->GetMainWindow();
+		Vec2f frameSize = window->GetFrameBufferSize();
+		mainCamera.parameters.projection = ProjectionType::Perspective;
+		mainCamera.parameters.near = 0.1f;
+		mainCamera.parameters.far = 100.0f;
+		mainCamera.parameters.height = Math::DegreesToRadians(45.0f);
+		mainCamera.parameters.SetAspectRatio(frameSize.x, frameSize.y);
+	}
+
+	// Load mesh and material for our additional objects
+
+	StringRef meshPath("res/models/sphere.mesh");
+	MeshId meshId = meshManager->GetIdByPath(meshPath);
+
+	StringRef matPath("res/materials/deferred_geometry/flat_white.material.json");
+	MaterialId matId = materialManager->GetIdByPath(matPath);
+
+	if (meshId.IsValid() && !matId.IsNull())
+	{
+		RenderOrderData renderOrderData;
+		renderOrderData.material = matId;
+		renderOrderData.transparency = materialManager->GetMaterialData(matId).transparency;
+
+		const float dist = 1.0f;
+		float a = Math::Const::Pi;
+
+		// Add a bunch of objects to the world
+		for (unsigned int i = 0, count = 1000; i < count; ++i)
+		{
+			float r = 6.8f + a / Math::Const::Tau * dist;
+			a += dist / r;
+
+			Entity entity = entityManager->Create();
+
+			SceneObjectId sceneObject = scene->AddSceneObject(entity);
+			Mat4x4f transform = Mat4x4f::Translate(Vec3f(std::sin(a) * r, 0.4f, std::cos(a) * r));
+			scene->SetLocalTransform(sceneObject, transform);
+
+			RenderObjectId renderObj = renderer->AddRenderObject(entity);
+			renderer->SetOrderData(renderObj, renderOrderData);
+			renderer->SetMeshId(renderObj, meshId);
+		}
+	}
 }
 
 void App::Update()
