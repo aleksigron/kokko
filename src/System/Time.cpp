b92a81bbcf0d2ae2c900f52f597cc09cd19c9b61
Aleksi Grön <aleksi.gron@gmail.com>
2021-03-11 23:35:32 +0200

    Add min and max limits to delta time calculation

--- src/System/Time.cpp
+++ src/System/Time.cpp
@@ -1,27 +1,32 @@
 #include "System/Time.hpp"
 
+#include <cmath>
+
 #include "System/IncludeGLFW.hpp"
 
 Time* Time::instance = nullptr;
+const double Time::MinDeltaTime = 0.0001;
+const double Time::MaxDeltaTime = 0.1;
 
-Time::Time() :
-	frameNumber(0),
-	frameStart(glfwGetTime()),
-	currentDeltaFloat(0.0f)
+Time::Time()
 {
-	this->currentTime = frameStart;
+	frameNumber = 0;
+	frameStart = glfwGetTime();
+	currentTime = frameStart;
+	currentDelta = MinDeltaTime;
+	currentDeltaFloat = static_cast<float>(currentDelta);
 
 	Time::instance = this;
 }
 
 void Time::Update()
 {
-	double previousTime = this->frameStart;
-	this->frameStart = glfwGetTime();
+	double previousTime = frameStart;
+	frameStart = glfwGetTime();
 	
-	this->currentTime = this->frameStart;
-	this->currentDelta = this->frameStart - previousTime;
-	this->currentDeltaFloat = static_cast<float>(this->currentDelta);
+	currentTime = frameStart;
+	currentDelta = std::fmin(std::fmax(frameStart - previousTime, MinDeltaTime), MaxDeltaTime);
+	currentDeltaFloat = static_cast<float>(currentDelta);
 
-	++this->frameNumber;
+	++frameNumber;
 }
--- src/System/Time.hpp
+++ src/System/Time.hpp
@@ -4,8 +4,13 @@ class Time
 {
 private:
 	static Time* instance;
+	static const double MinDeltaTime;
+	static const double MaxDeltaTime;
 
+	// 32-bit frame number can roll over if you run at 1000 fps for 50 days
+	// TODO: Should this be upgraded to 64-bit?
 	unsigned int frameNumber;
+
 	double frameStart;
 	double currentTime;
 	double currentDelta;
