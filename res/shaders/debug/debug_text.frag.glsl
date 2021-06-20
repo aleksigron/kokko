2b1cba186063eb48ba0ff9753c406fdaa2024750
Aleksi Grön <aleksi.gron@gmail.com>
2021-06-20 14:50:33 +0300

    Improve legibility of debug texts

--- res/shaders/debug/debug_text.frag.glsl
+++ res/shaders/debug/debug_text.frag.glsl
@@ -1,20 +1,17 @@
-in vec2 fs_tex_coord;
+in VS_TO_FS
+{
+	vec2 tex_coord;
+}
+fs_in;
 
 out vec4 color;
 
 uniform sampler2D glyph_tex;
 
-layout(std140, binding = BLOCK_BINDING_OBJECT) uniform DebugTextBlock
-{
-	vec2 shadow_offset;
-};
-
 void main()
 {
-	float normal = texture(glyph_tex, fs_tex_coord).r;
-	float offset = texture(glyph_tex, fs_tex_coord - shadow_offset).r;
+	float val = texture(glyph_tex, fs_in.tex_coord).r;
+	float alpha = 0.4 + val * 0.6;
 
-	float alpha = clamp(normal + offset, 0.0, 1.0);
-
-	color = vec4(normal, normal, normal, alpha);
+	color = vec4(val, val, val, alpha);
 }
--- res/shaders/debug/debug_text.vert.glsl
+++ res/shaders/debug/debug_text.vert.glsl
@@ -1,10 +1,14 @@
 layout(location = VERTEX_ATTR_INDEX_POS) in vec3 vert_pos;
 layout(location = VERTEX_ATTR_INDEX_UV0) in vec2 vert_tex;
 
-out vec2 fs_tex_coord;
+out VS_TO_FS
+{
+	vec2 tex_coord;
+}
+vs_out;
 
 void main()
 {
 	gl_Position = vec4(vert_pos, 1.0);
-	fs_tex_coord = vert_tex;
+	vs_out.tex_coord = vert_tex;
 }
