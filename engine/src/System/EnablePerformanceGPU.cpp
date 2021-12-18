ca40f099c73e46813a0a044764fb0f0567c6fc72
Aleksi Grön <aleksi.gron@gmail.com>
2021-12-18 17:26:55 +0200

    Request performance GPU on multi-GPU system

---
+++ engine/src/System/EnablePerformanceGPU.cpp
@@ -0,0 +1,6 @@
+
+#ifdef _WIN32
+#include <windows.h>
+extern "C" __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+extern "C" __declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 0x00000001;
+#endif
