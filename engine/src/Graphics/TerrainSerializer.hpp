bc7f7b4c9ab0e2beff35166bdb6dc651df4e6f1a
Aleksi Grön <aleksi.gron@gmail.com>
2022-02-06 23:45:13 +0200

    Terrain textures are serialized

--- engine/src/Graphics/TerrainSerializer.hpp
+++ engine/src/Graphics/TerrainSerializer.hpp
@@ -3,19 +3,22 @@
 #include "yaml-cpp/yaml.h"
 
 #include "Core/Hash.hpp"
+#include "Core/Uid.hpp"
 
 #include "Engine/ComponentSerializer.hpp"
 #include "Engine/Entity.hpp"
 
 #include "Graphics/TerrainSystem.hpp"
 
+#include "Resources/TextureManager.hpp"
 #include "Resources/YamlCustomTypes.hpp"
 
 class TerrainSerializer final : public ComponentSerializer
 {
 public:
-	TerrainSerializer(kokko::TerrainSystem* terrainSystem) :
-		terrainSystem(terrainSystem)
+	TerrainSerializer(kokko::TerrainSystem* terrainSystem, TextureManager* textureManager) :
+		terrainSystem(terrainSystem),
+		textureManager(textureManager)
 	{
 	}
 
@@ -45,6 +48,40 @@ public:
 			terrain.heightRange = maxNode.as<float>();
 
 		kokko::TerrainId id = terrainSystem->AddTerrain(entity, terrain);
+
+		YAML::Node albedoNode = map["albedo_texture"];
+		if (albedoNode.IsDefined() && albedoNode.IsScalar())
+		{
+			const std::string& uidStr = albedoNode.Scalar();
+			auto uidOpt = kokko::Uid::FromString(ArrayView(uidStr.c_str(), uidStr.length()));
+			if (uidOpt.HasValue())
+			{
+				TextureId textureId = textureManager->FindTextureByUid(uidOpt.GetValue());
+
+				if (textureId != TextureId::Null)
+				{
+					const TextureData& texture = textureManager->GetTextureData(textureId);
+					terrainSystem->SetAlbedoTexture(id, textureId, texture.textureObjectId);
+				}
+			}
+		}
+
+		YAML::Node roughnessNode = map["roughness_texture"];
+		if (roughnessNode.IsDefined() && roughnessNode.IsScalar())
+		{
+			const std::string& uidStr = roughnessNode.Scalar();
+			auto uidOpt = kokko::Uid::FromString(ArrayView(uidStr.c_str(), uidStr.length()));
+			if (uidOpt.HasValue())
+			{
+				TextureId textureId = textureManager->FindTextureByUid(uidOpt.GetValue());
+
+				if (textureId != TextureId::Null)
+				{
+					const TextureData& texture = textureManager->GetTextureData(textureId);
+					terrainSystem->SetRoughnessTexture(id, textureId, texture.textureObjectId);
+				}
+			}
+		}
 	}
 
 	virtual void SerializeComponent(YAML::Emitter& out, Entity entity) override
@@ -65,10 +102,31 @@ public:
 			out << YAML::Key << "terrain_bottom" << YAML::Value << heightOrigin;
 			out << YAML::Key << "terrain_height" << YAML::Value << heightRange;
 
+			char uidStr[kokko::Uid::StringLength + 1];
+
+			TextureId albedoTextureId = terrainSystem->GetAlbedoTextureId(terrainId);
+			if (albedoTextureId != TextureId::Null)
+			{
+				const TextureData& texture = textureManager->GetTextureData(albedoTextureId);
+				texture.uid.WriteTo(ArrayView(uidStr));
+				uidStr[kokko::Uid::StringLength] = '\0';
+				out << YAML::Key << "albedo_texture" << YAML::Value << uidStr;
+			}
+
+			TextureId roughnessTextureId = terrainSystem->GetRoughnessTextureId(terrainId);
+			if (roughnessTextureId != TextureId::Null)
+			{
+				const TextureData& texture = textureManager->GetTextureData(roughnessTextureId);
+				texture.uid.WriteTo(ArrayView(uidStr));
+				uidStr[kokko::Uid::StringLength] = '\0';
+				out << YAML::Key << "roughness_texture" << YAML::Value << uidStr;
+			}
+
 			out << YAML::EndMap;
 		}
 	}
 
 private:
 	kokko::TerrainSystem* terrainSystem;
+	TextureManager* textureManager;
 };
