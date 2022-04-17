f981e3d83d134db46e165792120e006900ee2aac
Aleksi Grön <aleksi.gron@gmail.com>
2022-04-17 13:43:43 +0300

    Add assertions and tests to BitfieldVariable

---
+++ engine/src/Core/BitfieldVariable.cpp
@@ -0,0 +1,44 @@
+#include "Core/BitfieldVariable.hpp"
+
+#include <cstdint>
+
+#include "doctest/doctest.h"
+
+TEST_CASE("BitfieldVariable.GetValue")
+{
+	BitfieldVariable<uint32_t> var0;
+	var0.SetDefinition(10, sizeof(uint32_t) * 8);
+
+	BitfieldVariable<uint32_t> var1;
+	var1.SetDefinition(22, var0.shift);
+
+	uint32_t val = 0;
+	var0.AssignValue(val, 1023);
+	var1.AssignValue(val, 11888);
+
+	CHECK(var0.GetValue(val) == 1023);
+	CHECK(var1.GetValue(val) == 11888);
+}
+
+TEST_CASE("BitfieldVariable.AssignZero")
+{
+	BitfieldVariable<uint32_t> var0;
+	var0.SetDefinition(10, sizeof(uint32_t) * 8);
+
+	BitfieldVariable<uint32_t> var1;
+	var1.SetDefinition(22, var0.shift);
+
+	uint32_t val = 0;
+	var0.AssignValue(val, 1023);
+	var1.AssignValue(val, 11888);
+
+	var0.AssignZero(val);
+
+	CHECK(var0.GetValue(val) == 0);
+	CHECK(var1.GetValue(val) == 11888);
+
+	var1.AssignZero(val);
+
+	CHECK(var0.GetValue(val) == 0);
+	CHECK(var1.GetValue(val) == 0);
+}
