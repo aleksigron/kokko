a715ae627d74c293db547220b47f23c50a241685
Aleksi Grön <aleksi.gron@gmail.com>
2023-07-11 19:35:04 +0300

    Add ability to create new material assets in asset browser

--- editor/src/App/EditorConstants.cpp
+++ editor/src/App/EditorConstants.cpp
@@ -11,5 +11,24 @@ const char* const EditorConstants::AssetDirectoryName = "Assets";
 const char* const EditorConstants::SceneDragDropType = "SceneObject";
 const char* const EditorConstants::AssetDragDropType = "Asset";
 const char* const EditorConstants::ModelMeshDragDropType = "ModelMesh";
+
+const char* const EditorConstants::NewAssetContentMaterial = R"RAW({
+	"shader": "beefdc5ec8726697d0282955735586ec",
+	"variables": [
+		{
+			"name": "color_tint",
+			"value": [ 0.8, 0.8, 0.8 ]
+		},
+		{
+			"name": "metalness",
+			"value": 0.0
+		},
+		{
+			"name": "roughness",
+			"value": 0.3
+		}
+	]
+}
+)RAW";
 }
 }
